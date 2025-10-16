ad058c17f0ce1e909f3eb5fc0fd441b048a2b50f
suhakotlin <tngk0011@gmail.com>
2025-10-16 15:48:43 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,23 +18,23 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(21)  // 네 환경에 맞게 유지
+    jvmToolchain(21)  
 }
 
 application {
-    // Main.kt 의 진입점 (패키지 기준
+    
     mainClass.set("MainKt")
 }
 tasks.jar {
-    // META-INF 충돌 방지
+    
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
-    // JAR에 Main-Class 기록 (java -jar 가능)
+    
     manifest {
         attributes["Main-Class"] = application.mainClass.get()
     }
 
-    // 런타임 의존성까지 전부 포함해서 하나의 JAR로 만든다 (fat/uber JAR)
+    
     from(
         configurations.runtimeClasspath.get().map { file ->
             if (file.isDirectory) file else zipTree(file)
